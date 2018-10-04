a6a2b0fa1af8c332131320be01799f7b3f4b4db3
Fahad Qazi <fahadqazi@gmail.com>
2018-10-04 09:30:50 +0100

    making chart changes

--- src/App.js
+++ src/App.js
@@ -1,74 +1,91 @@
-import React, { Component } from 'react';
-import ScatterPlot from './ScatterPlot';
-import LineChart from './LineChart';
-import * as d3 from 'd3'
-import './App.css';
+import React, { Component } from "react";
+import ScatterPlot from "./ScatterPlot";
+import LineChart from "./LineChart";
+import * as d3 from "d3";
+import "./App.css";
 
-const data = d3.range(100).map(_ => [Math.random(), Math.random()])
+const data = d3.range(100).map(_ => [Math.random(), Math.random()]);
 
-const genData = function(){
-  return d3.range(100).map(_ => [Math.random(), Math.random()])
-}
+const genData = function() {
+  return d3.range(100).map(_ => [Math.random(), Math.random()]);
+};
 
 const randomNumbers = (min, max) => {
-  return Math.floor(Math.random() * (max - min) + min)
-}
+  return Math.floor(Math.random() * (max - min) + min);
+};
 
-const genTimeData = function(){
-  const arr = []
-  const duration = 500
-  const maxLength = 100
-  const now = new Date()
-  for (let i=0; i<maxLength; i++){
+const genTimeData = function() {
+  const arr = [];
+  const duration = 500;
+  const maxLength = 100;
+  const now = new Date();
+  for (let i = 0; i < maxLength; i++) {
     arr.push({
       x: new Date(now.getTime() - (maxLength - i) * duration),
       y: randomNumbers(90, 100)
-    })
+    });
   }
   return arr;
-}
+};
 
-console.log('GenTimeDate: ', genTimeData())
+// console.log("GenTimeDate: ", genTimeData());
 
 class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       width: 300,
       height: 200
-    }
-    this.handleClick = this.handleClick.bind(this)
+    };
+    // this.handleClick = this.handleClick.bind(this);
   }
-  handleClick(){
-    this.setState({
-      data: genData()
-    })
-  }
+  // handleClick() {
+  //   this.setState({
+  //     data: genData()
+  //   });
+  // }
 
-  tick(){
+  tick() {
     this.setState({
       data: genTimeData()
-    })
+    });
   }
 
-  componentDidMount(){
-    this.timerId = setInterval(
-      () => this.tick(),
-      2000
-    )
+  componentDidMount() {
+    this.timerId = setInterval(() => this.tick(), 2000);
   }
 
-  componentWillUnmount(){
-    clearInterval(this.timerId)
+  componentWillUnmount() {
+    clearInterval(this.timerId);
   }
 
   render() {
     // console.log('this.state: ', this.state)
     return (
       <div className="App">
-        <svg width={800} height={800} onClick={this.handleClick}>
-          {/*<ScatterPlot className='chart' x={50} y={50} data={genData()} width={this.state.width} height={this.state.height}/>*/}
-            <LineChart className='chart' x={50} y={50} data={genTimeData()} width={this.state.width} height={this.state.height}/>
+        {/* <svg width={800} height={800} onClick={this.handleClick}> */}
+        <svg width={800} height={800}>
+          {/* <ScatterPlot
+            className="chart"
+            x={50}
+            y={50}
+            data={genData()}
+          {/* <ScatterPlot
+            className="chart"
+            x={50}
+            y={50}
+            data={genData()}
+            width={this.state.width}
+            height={this.state.height}
+          /> */}
+          <LineChart
+            className="chart"
+            x={50}
+            y={50}
+            data={genTimeData()}
+            width={this.state.width}
+            height={this.state.height}
+          />
         </svg>
       </div>
     );
--- src/LineChart.js
+++ src/LineChart.js
@@ -1,44 +1,60 @@
-import React, {Component} from 'react'
-import * as d3 from 'd3';
-import Axis from './Axis'
-import './App.css';
+import React, { Component } from "react";
+import * as d3 from "d3";
+import Axis from "./Axis";
+import "./App.css";
 
-class LineChart extends Component{
-  constructor(props){
-    super(props)
+class LineChart extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      xScale: d3.scaleTime().domain([d3.min(this.props.data, d => d.x, d3.max(this.props.data, d => d.x))]).range([0, this.props.width]),
-      yScale: d3.scaleLinear().domain([d3.min(this.props.data, d => d.y), d3.max(this.props.data, d => d.y)]).range([this.props.height, 0])
-    }
+      xScale: d3
+        .scaleTime()
+        .domain([
+          d3.min(this.props.data, d => d.x, d3.max(this.props.data, d => d.x))
+        ])
+        .range([0, this.props.width]),
+      yScale: d3
+        .scaleLinear()
+        .domain([
+          d3.min(this.props.data, d => d.y),
+          d3.max(this.props.data, d => d.y)
+        ])
+        .range([this.props.height, 0])
+    };
   }
 
-  static getDerivedStateFromProps(props, state){
-    let {xScale, yScale} = state
-    let {data} = props
-    console.log('Data from line chart: ', data)
-    xScale.domain([d3.min(props.data, d => d.x), d3.max(props.data, d => d.x)])
-    yScale.domain([d3.min(props.data, d => d.y), d3.max(props.data, d => d.y)])
+  static getDerivedStateFromProps(props, state) {
+    let { xScale, yScale } = state;
+    let { data } = props;
+    // console.log("Data from line chart: ", data);
+    xScale.domain([d3.min(props.data, d => d.x), d3.max(props.data, d => d.x)]);
+    yScale.domain([d3.min(props.data, d => d.y), d3.max(props.data, d => d.y)]);
 
-    return {state, xScale, yScale, data}
+    return { state, xScale, yScale, data };
   }
 
-  get line(){
-    return d3.line()
+  get line() {
+    return d3
+      .line()
       .curve(d3.curveBasis)
       .x(d => this.state.xScale(d.x))
-      .y(d => this.state.yScale(d.y))
+      .y(d => this.state.yScale(d.y));
   }
 
-  render(){
-    const {x, y, width, height} = this.props
-    const {xScale, yScale} = this.state
-    return(
+  componentDidUpdate() {
+    console.log("component did update");
+  }
+
+  render() {
+    const { x, y, width, height } = this.props;
+    const { xScale, yScale } = this.state;
+    return (
       <g width={width} height={height} transform={`${x}, ${y}`}>
-        <path className='line' d={this.line(this.state.data)}/>
-        <Axis x={0} y={height} type='Bottom' scale={xScale}/>
-        <Axis x={0} y={0} type='Left' scale={yScale}/>
+        <path className="line" d={this.line(this.state.data)} />
+        <Axis x={0} y={height} type="Bottom" scale={xScale} />
+        <Axis x={0} y={0} type="Left" scale={yScale} />
       </g>
-    )
+    );
   }
 }
 
